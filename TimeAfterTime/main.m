d6eb91981658ebf4b60a9f29960ae812941c1a0c
Edwin Cardenas <eacardenase@gmail.com>
2024-12-30 14:51:57 -0500

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Edwin Cardenas on 12/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
