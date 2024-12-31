93066858eafd6dbc40ca24bfe98c89e46718757b
Edwin Cardenas <eacardenase@gmail.com>
2024-12-31 12:14:12 -0500

    feat: class vs instance methods

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
         NSDate *now = [NSDate date];
         
         NSLog(@"This NSDate object lives at %p", now);
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"I has been %f seconds since the start of 1970.", seconds);
     }
     return 0;
 }
