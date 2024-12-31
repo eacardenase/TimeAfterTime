e68c8c81a8e4eb87980d7c715ffd1c0a86d1c637
Edwin Cardenas <eacardenase@gmail.com>
2024-12-31 12:37:25 -0500

    feat: wrong messages on class and instance methods

--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -16,6 +16,13 @@ int main(int argc, const char * argv[]) {
         
         double seconds = [now timeIntervalSince1970];
         NSLog(@"I has been %f seconds since the start of 1970.", seconds);
+        
+        // Sending bogus messages to see errors...
+//        double testSeconds = [NSDate timeIntervalSince1970];
+//        NSDate *testNow = [now date];
+        
+        // Mistyped selector name
+//        testSeconds = [now fooIntervalSince1970];
     }
     return 0;
 }
